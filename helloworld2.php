de230fd43d483fb5f32bda44cee80b5ea39dd2f2
Sweet-Tooth-Lab <tprofessional505@gmail.com>
2025-01-25 23:05:04 -0700

    Add files via upload
    This has comments ( and a snowman =D )

---
+++ helloworld2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+// this is a comment that's with two slashes
+# this is a comment that's with a hashtag
+echo "Hello World!";
+/*
+This is a multiline comment using slash and asteriks
+Also here is a (decent apempt at a) snow man:
+
+  _[=]_
+   |>|
+   ---
+  | : |
+ _/---\_
+/   :   \
+|   :   |
+\_______/
+
+*/
+?>
+
+</body>
+</html>
