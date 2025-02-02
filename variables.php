4be96182bf8b207dbc45a3cd3465531a4d555387
Sweet-Tooth-Lab <tprofessional505@gmail.com>
2025-02-02 02:25:32 -0700

    Add files via upload

---
+++ variables.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Variables</title>
+</head>
+<body>
+<pre>
+<?php // Script 2.3 - variables.php
+
+// Setting up line breaks
+const BR = '<br />';
+
+// An address:
+$_first_name = 'Troy';
+$_last_name = 'Rehborg';
+$_address = '222 Secondary Street, Citytown, OR, 22222'; 
+$_phone_number = '(314)-159-2653';
+// phone number is just the digets of pi
+
+print $_first_name;
+print BR;
+print $_last_name;
+print BR;
+print $_address;
+print BR;
+print $_phone_number;
+
+?>
+</pre>
+</body>
+</html>
