9af15600f3b84cddb67da75bad44c7920f2338c4
Sweet-Tooth-Lab <tprofessional505@gmail.com>
2025-02-08 03:55:11 -0700

    Add files via upload

---
+++ randon_number.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<html>
+<body>
+
+<?php
+
+$_random_number = rand(10,100);
+print "Here's a random number between 10 and 100: $_random_number";
+
+?>
+
+</body>
+</html>
