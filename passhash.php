0a4d13759b6403abfe4413e7e58c9dcb72e926d0
Sweet-Tooth-Lab <tprofessional505@gmail.com>
2025-02-09 02:45:35 -0700

    Add files via upload

--- passhash.php
+++ passhash.php
@@ -9,6 +9,6 @@ echo  $hashed_password;
 // Can you think of how to use this as a password verification?
 /* This can be used for password validation as the password gets encrypted before sending
 than it can be compaired to the recorded encrypted password in the website server to see if
-they are an exact match of eachother.*/
+they are an exact match of eachother without the plain text getting exposed.*/
 
 ?>
