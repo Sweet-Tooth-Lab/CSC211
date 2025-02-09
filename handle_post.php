8fb093916fa2ee2070962da79b3b719edb76b424
Sweet-Tooth-Lab <tprofessional505@gmail.com>
2025-02-09 02:07:05 -0700

    Add files via upload

--- handle_post.php
+++ handle_post.php
@@ -5,27 +5,21 @@
 	<title>Forum Posting</title>
 </head>
 <body>
-<?php // Script 5.4 - handle_post.php
+<?php // Script 5.2 - handle_post.php
 /* This script recieves five values from posting.html:
 first_name, last_name, email, posting, submit*/
 
 // Get the values from the $_POST array:
 $_first_name = $_POST['first_name'];
 $_last_name = $_POST['last_name'];
-$_posting = nl2br($_POST['posting'], false);
+$_posting = $_POST['posting'];
 
 // Create a full name variable:
 $_name = $_first_name . ' ' . $_last_name;
 
-// Adjust for HTML tags:
-$html_post = htmlentities($_POST['posting']);
-$strip_post = strip_tags($_POST['posting']);
-
 // Print a message:
-print "<div>Thank you, $_name, for your Posting:
-<p>Original: $_posting</p>
-<p>Entity: $html_post</p>
-<p>Stripped: $strip_post</p></div>";
+print "<div>Thank you, $_name, for your Posting
+<p>$_posting</p></div>";
 
 ?>
 </body>
